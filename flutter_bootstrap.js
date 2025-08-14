0c3f6c99d4680161a79699468fedfb400f42f32e
Aswin <120723848+Aswinraj040@users.noreply.github.com>
2025-08-14 19:59:44 +0530

    New_Changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "237591627"
+    serviceWorkerVersion: "1909571843"
   }
 });
